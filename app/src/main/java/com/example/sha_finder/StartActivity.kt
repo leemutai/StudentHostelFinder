26f7e10160e8f497dd453d503a9849c7f2528df6
LEE MUTAI <71839051+leemutai@users.noreply.github.com>
2024-02-15 16:39:31 +0300

    created login activity

--- app/src/main/java/com/example/sha_finder/StartActivity.kt
+++ app/src/main/java/com/example/sha_finder/StartActivity.kt
@@ -1,11 +1,20 @@
 package com.example.sha_finder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sha_finder.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
